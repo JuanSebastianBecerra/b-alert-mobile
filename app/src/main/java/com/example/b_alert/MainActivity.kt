3537d79b9531cd9757d4d8979a647703c4ef2040
Nestor Pérez Espinel <neztoring@gmail.com>
2023-09-28 01:04:40 -0500

    Listar Alarmas

--- app/src/main/java/com/example/b_alert/MainActivity.kt
+++ app/src/main/java/com/example/b_alert/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.b_alert
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonIngresarClick = findViewById<Button>(R.id.button_ingresar)
+        buttonIngresarClick.setOnClickListener {
+            val intent = Intent(this, ListAlarm::class.java)
+            startActivity(intent)
+        }
+
     }
 }
