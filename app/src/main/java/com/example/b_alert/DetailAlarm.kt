21ccd983068b1be5a7839e2672a4114304c25b04
JuanSebastianB <j.becerrab@uniandes.edu.co>
2023-09-27 22:27:01 -0500

    feat: create alarm detail screen

---
+++ app/src/main/java/com/example/b_alert/DetailAlarm.kt
@@ -0,0 +1,12 @@
+package com.example.b_alert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAlarm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_alarm)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
